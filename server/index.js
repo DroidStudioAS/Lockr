b978338929fdc8420892b63cf3422badda1dea4a
DroidStudioAS <as.droiddevteam@gmail.com>
2024-02-28 15:45:32 +0100

    refactored serverside code

--- server/index.js
+++ server/index.js
@@ -2,10 +2,13 @@ import { Server } from "socket.io";
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from "url";
+import { text } from "stream/consumers";
+import { builtinModules } from "module";
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename);
 
+const ADMIN = "Admin";
 const PORT = process.env.PORT || 3500
 
 const app = express();
@@ -16,7 +19,13 @@ const expressServer = app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
 });
 
-
+//state for users
+const usersState = {
+    users: [],
+    setUsers: function (newUsers) {
+        this.users = newUsers;
+    }
+}
 
 const io = new Server(expressServer, {
     cors: {
@@ -29,23 +38,108 @@ io.on('connection', socket => {
     console.log(`User: ${socket.id} connected`)
 
     //send message to user that connected
-    socket.emit('message', 'Welcome to ChatApp')
+    socket.emit('message', buildMessage(ADMIN, "Welcome To Lockr").text)
     //notify all other users that user is connected
     socket.broadcast.emit('message', `User: ${socket.id.substring(0, 5)} Connected`);
 
+    socket.on('enterRoom', ({ name, room }) => {
+        //Leave a previous room
+        const prevRoom = getUser(socket.id)?.room;
+        if (prevRoom) {
+            socket.leave(prevRoom)
+            io.to(prevRoom).emit('message', buildMessage(ADMIN, `${name} has left`))
+        }
+
+        const user = activateUser(socket.id, name, room);
+        //cannot update previours room users list
+        if (prevRoom) {
+            io.to(prevRoom).emit('userList', {
+                users: getUsersInRoom(prevRoom)
+            })
+        }
+
+        socket.join(user.room)
+        //to user who joined
+        socket.emit('message', buildMessage(ADMIN, `You Have Joined ${user.room} Chat`));
+        //to everyone else
+        socket.broadcast.to(user.room).emit('message', buildMessage(ADMIN, `User ${user.name} Joined The Room`));
+        //update user list for the active room
+        io.to(user.room).emit('userlist', {
+            users: getUsersInRoom(user.room)
+        });
+        //update the active room list
+        io.emit('roomList', {
+            rooms: getAllActiveRooms()
+        })
+    })
+    //listening for disconnection
+    socket.on('disconnect', () => {
+        const user = getUser(socket.id);
+        userLeft(socket.id);
+
+        if (user) {
+            io.to(user.room).emit('message', buildMessage(ADMIN, `${user.name} left the room`));
+            io.to(user.room).emit('userList', {
+                users: getUsersInRoom(user.room);
+            })
+            io.emit('roomList', {
+                rooms: getAllActiveRooms();
+            })
+        }
+    });
+
     //listening for a message event
-    socket.on('message', data => {
-        console.log(data);
+    socket.on('message', ({ name, text }) => {
+        const room = getUser(socket.id)?.room;
+        if (room) {
+            io.to(room).emit('message', buildMessage(name, text));
+        }
         io.emit('message', `${socket.id.substring(0, 5)}: ${data}`)
     })
 
-    //listening for disconnection
-    socket.on('disconnect', () => {
-        socket.broadcast.emit('message', `User: ${socket.id.substring(0, 5)} Disconnected`);
-    });
     //listen for activity
     socket.on('activity', (name) => {
+        const room = getUser(socket.id)?.room;
+        if (room) {
+            socket.broadcast.to(room).emit('activity', name);
+        }
         socket.broadcast.emit('activity', name.substring(0, 5))
     })
 })
+
+function buildMessage(name, text) {
+    return {
+        'name': name,
+        "text": text,
+        "time": new Intl.DateTimeFormat('default', {
+            hour: 'numeric',
+            minute: 'numeric',
+            second: 'numeric'
+        }).format(new Date())
+    }
+}
+//user functions
+function activateUser(id, name, room) {
+    const user = { id, name, room }
+    usersState.setUsers([
+        ...usersState.users.filter(user => user.id !== id),
+        user
+    ])
+    return user;
+}
+function userLeft(id) {
+    usersState.setUsers(
+        usersState.users.filter(user => user.id !== id)
+    )
+}
+function getUser(id) {
+    return usersState.users.find(user => user.id === id);
+}
+function getUsersInRoom(room) {
+    return usersState.users.filter(user => user.room === room);
+}
+function getAllActiveRooms() {
+    return Array.from(new Set(usersState.users.map(user => user.room)))
+
+}
 
--- server/public/app.js
+++ server/public/app.js
@@ -1,24 +1,47 @@
 
 const socket = io('ws://localhost:3500')
 
+const msgInput = document.querySelector('#message');
+const nameInput = document.querySelector('#name');
+const chatRoom = document.querySelector('#room');
+
 const activity = document.querySelector('.activity');
-const msgInput = document.querySelector('input');
+const usersList = document.querySelector('.user-list');
+const roomList = document.querySelector('.room-list');
+const chatDisplay = document.querySelector('.chat-display');
 
 function sendMessage(e) {
     e.preventDefault();
-    if (msgInput.value) {
-        socket.emit('message', msgInput.value);
+    if (msgInput.value && nameInput.value && chatRoom.value) {
+        socket.emit('message', {
+            text: msgInput.value,
+            name: nameInput.value,
+
+        });
         msgInput.value = ""
     }
     msgInput.focus();
 }
+function enterRoom() {
+    e.preventDefault()
+    if (nameInput.value && chatRoom.value) {
+        socket.emit('enterRoom', {
+            name: nameInput.value,
+            room: chatRoom.value
+        })
+    }
+}
 
-document.querySelector('form')
+//listeners
+document.querySelector('.form-msg')
     .addEventListener('submit', sendMessage);
-
+document.querySelector('.form-join')
+    .addEventListener('submit', enterRoom);
 msgInput.addEventListener('keypress', () => {
-    socket.emit('activity', socket.id.substring(0.5))
+    socket.emit('activity', nameInput.value)
 })
+
+
 
 let activityTimer
 socket.on('activity', (name) => {
